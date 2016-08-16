1f0f6793ca4b1985a60155b4636bb7d09e1f19ff
shawnnxiao <76001218@qq.com>
2016-08-16 18:55:01 +0800

    fixed text

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="page">
     <counter></counter>
-    <h1>Hello world!!!!</h1>
+    <h1>Hello world!</h1>
     <p>
-      To get started, edit files in <code>./client</code> and save.
+      To get started, edit files in <code>./src</code> and save.
     </p>
   </div>
 </template>
 
 <script>
-import Counter from '../components/Counter'
+  import Counter from '../components/Counter'
 
-export default {
-  components: {
-    Counter
+  export default {
+    components: {
+      Counter
+    }
   }
-}
 </script>
